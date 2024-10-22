912243057cb8d78c80bf88b4b9c2e0c1694c2d39
Mohri Samuel Muakpo <sammuaks@gmail.com>
2024-10-22 16:10:19 +0100

    Http GET

--- 3.1 Express Server/index.js
+++ 3.1 Express Server/index.js
@@ -2,6 +2,10 @@ import express from "express";
 const app = express();
 const port = 3000;
 
+app.get("/", (req, res) => {
+  res.send("Hello, I am Sam Muak's")
+});
+
 app.listen(port, () => {
   console.log(`Server running on port ${port}.`);
 });
