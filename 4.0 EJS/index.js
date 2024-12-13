8f7b4b8b333430b03430d08407b04149b250dcb5
Mohri Samuel Muakpo <sammuaks@gmail.com>
2024-12-13 16:03:37 +0100

    Add 4.0

--- 4.0 EJS/index.js
+++ 4.0 EJS/index.js
@@ -2,3 +2,29 @@ import express from "express";
 
 const app = express();
 const port = 3000;
+
+app.get ("/", (req, res) => {
+    const today = new Date();
+    const day = today.getDay();
+
+    let week = "a weekday";
+    let text = "it's time to work hard";
+
+    if (day === 0 || day === 6) {
+        week = "the weekend";
+        text = "it's time to have fun"
+    };
+
+    res.render ("index.ejs", {
+        dayType: week,
+        advice: text,
+    });
+})
+
+app.listen (port, () => {
+    console.log(`Server running on ${port}.`)
+})
+
+
+// const options = { weekday: "long" };
+// console.log(new Intl.DateTimeFormat("en-US", options).format(valentines));
