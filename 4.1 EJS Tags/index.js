6ad775917ef3e50ca7433d056413f959ead5e635
Mohri Samuel Muakpo <sammuaks@gmail.com>
2024-12-17 09:11:05 +0100

    EJS tags complete

--- 4.1 EJS Tags/index.js
+++ 4.1 EJS Tags/index.js
@@ -2,11 +2,13 @@ import express from "express";
 const app = express();
 const port = 3000;
 
+const fruit = ["apple", "banana", "cherry"];
+
 app.get("/", (req, res) => {
   const data = {
     title: "EJS Tags",
     seconds: new Date().getSeconds(),
-    items: ["apple", "banana", "cherry"],
+    items: fruit,
     htmlContent: "<strong>This is some strong text</strong>",
   };
   res.render("index.ejs", data);
